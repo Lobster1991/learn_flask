a0e5aaf5409e4f06a30eca9a91758fc4eee05adc
Lobster <shichao3712@hotmail.com>
2016-04-10 21:59:56 +0800

    添加数据库迁移

--- hello.py
+++ hello.py
@@ -6,9 +6,10 @@ from flask.ext.moment import Moment
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from datetime import datetime
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+from flask.ext.script import Manager, Shell
+from flask.ext.migrate import Migrate, MigrateCommand
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -27,6 +28,9 @@ app.config['SQLACHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[DataRequired()])
@@ -43,7 +47,7 @@ class Role(db.Model):
     # 'User'表明这个关系的另一端的模型是'User’。
     # backref参数像'User'模型中添加一个'role'属性，从而定义反向关系。这一 \
     # 属性可替代role_id访问Role模型，此时获取的是模型对象，而不是外键的值。
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     # 注意这个'__repr__()'方法，常与'__str__()'方法比较
     def __repr__(self):
@@ -64,17 +68,30 @@ class User(db.Model):
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        # post方法重定向get方法
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template("index.html",form=form, name=session.get('name'),
-                           current_time=datetime.utcnow())
+    return render_template('index.html',
+                           form = form, name = session.get('name'),
+                           known = session.get('known', False))
+
+    #     old_name = session.get('name')
+    #     if old_name is not None and old_name != form.name.data:
+    #         flash('Looks like you have changed your name!')
+    #     # post方法重定向get方法
+    #     session['name'] = form.name.data
+    #     return redirect(url_for('index'))
+    # return render_template("index.html",form=form, name=session.get('name'),
+    #                        current_time=datetime.utcnow())
                             # name信息被保存在session中，session跟普通字典一样，
                             # 对于不存在的键，get()会返回默认值None。
 
@@ -91,5 +108,10 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template("500.html"), 500
 
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.debug = True
+    manager.add_command("shell", Shell(make_context=make_shell_context))
+    manager.run()
